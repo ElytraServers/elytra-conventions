9d2c470d602fac9c93d28d3555672270a1ae2177
楓 <r0yalist@outlook.com>
2026-06-13 03:47:51 +0800

    Add publishing to maven central (#21)

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -2,11 +2,13 @@ plugins {
 	`java-gradle-plugin`
 	`kotlin-dsl`
 	`maven-publish`
+	signing
 
 	id("com.palantir.git-version") version "5.0.0"
 	id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.18.1"
 
 	id("com.gradle.plugin-publish") version "2.1.1"
+	id("com.vanniktech.maven.publish") version "0.36.0"
 }
 
 val gitVersion: groovy.lang.Closure<String> by extra
@@ -65,4 +67,42 @@ kotlin {
 apiValidation {
 	ignoredPackages += listOf("cn.elytra.gradle.conventions.internal")
 	apiDumpDirectory = "api"
+}
+
+tasks.withType<Javadoc> {
+	val opt = options as StandardJavadocDocletOptions
+	// don't panic with missing docs.
+	opt.addStringOption("Xdoclint:all,-missing", "-quiet")
+}
+
+mavenPublishing {
+	coordinates("cn.elytra", "elytra-conventions", project.version.toString().removePrefix("v"))
+
+	pom {
+		name.set("Elytra Conventions Plugins")
+		description.set("A convention Gradle plugin for GTNewHorizons specific region.")
+		inceptionYear.set("2026")
+		url.set("https://github.com/ElytraServers/elytra-conventions")
+		licenses {
+			license {
+				name.set("MIT License")
+				url.set("https://github.com/ElytraServers/elytra-conventions/blob/master/LICENSE")
+			}
+		}
+		developers {
+			developer {
+				id.set("taskeren")
+				name.set("Taskeren")
+				email.set("r0yalist@outlook.com")
+			}
+		}
+		scm {
+			connection.set("scm:git:git://github.com/ElytraServers/elytra-conventions.git")
+			developerConnection.set("scm:git:ssh://github.com/ElytraServers/elytra-conventions.git")
+			url.set("https://github.com/ElytraServers/elytra-conventions")
+		}
+	}
+
+	publishToMavenCentral()
+	signAllPublications()
 }
